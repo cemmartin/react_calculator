f139d902497988851d5d5dd6e64a0dcee567f33d
Claire Martin <claire.e.martin15@gmail.com>
2024-01-21 22:15:43 +0000

    some basic fixes, upgraded the readme, and a bit of styling

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
-# Getting Started with Create React App
+To run this calculator, type the following commands in your terminal:
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+`npm install`
+`npm start`
 
-## Available Scripts
+The app should then launch in [http://localhost:3000/]
 
-In the project directory, you can run:
 
-### `npm start`
+To test the application, run the following commands in your terminal in the same folder as the project:
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+`npm test`
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+You should see the tests run in your terminal
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+To open Cypress for further testing, run the following command in your terminals:
 
-### `npn run test:e2e`
+`npm run test:e2e`
 
-Lanches Cyoress test suite.
+This should launch Cypress
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -4,6 +4,8 @@ import { render, fireEvent } from "@testing-library/react";
 
 describe("Calculator", () => {
   let container;
+  // can declare all the variables here, i.e. (below) --> makes it easier to read
+  // let button1 = container.getByTestId('number0')
   beforeEach(() => {
     container = render(<Calculator />);
   });
@@ -15,7 +17,7 @@ describe("Calculator", () => {
     expect(runningTotal.textContent).toEqual("4");
   });
 
-  it("should add 1 + 4 together to get 5", () => {
+  it("should add 1 and 4 together to get 5", () => {
     const button1 = container.getByTestId("number1");
     // fireEvent.click(button1);
     // expect(runningTotal.textContent).toEqual('5');
@@ -43,7 +45,7 @@ describe("Calculator", () => {
     expect(runningTotal.textContent).toEqual("3");
   });
 
-  it('should multiply 3 by 5 and get 15', () => {
+  it("should multiply 3 by 5 and get 15", () => {
     const button3 = container.getByTestId("number3");
     const operator_multiply = container.getByTestId("operator-multiply");
     const button5 = container.getByTestId("number5");
@@ -54,9 +56,8 @@ describe("Calculator", () => {
     fireEvent.click(button5);
     fireEvent.click(operator_equals);
     expect(runningTotal.textContent).toEqual("15");
-
-  })
-  it('should divide 21 by 7 and get 3', () => {
+  });
+  it("should divide 21 by 7 and get 3", () => {
     const button2 = container.getByTestId("number2");
     const button1 = container.getByTestId("number1");
     const operator_divide = container.getByTestId("operator-divide");
@@ -68,19 +69,17 @@ describe("Calculator", () => {
     fireEvent.click(operator_divide);
     fireEvent.click(button7);
     fireEvent.click(operator_equals);
-    expect(runningTotal.textContent).toEqual("3")
-
-  })
-  it('should concatenate multiple button clicks', () => {
+    expect(runningTotal.textContent).toEqual("3");
+  });
+  it("should concatenate multiple button clicks", () => {
     const button3 = container.getByTestId("number3");
     const button8 = container.getByTestId("number8");
     const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button3);
     fireEvent.click(button8);
     expect(runningTotal.textContent).toEqual("38");
-
-  })
-  it('should chain multiple operations together', () => {
+  });
+  it("should chain multiple operations together", () => {
     const button8 = container.getByTestId("number8");
     const operator_divide = container.getByTestId("operator-divide");
     const button4 = container.getByTestId("number4");
@@ -93,15 +92,13 @@ describe("Calculator", () => {
     fireEvent.click(operator_subtract);
     fireEvent.click(button1);
     expect(runningTotal.textContent).toEqual("1");
-  })
-  it('should clear the running total w/o affecting the calculation', () => {
+  });
+  it("should clear the running total w/o affecting the calculation", () => {
     const button9 = container.getByTestId("number9");
     const clear = container.getByTestId("clear");
     const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button9);
-    fireEvent.click(clear)
-    expect(runningTotal.textContent).toEqual("0")
-  })
-
-
+    fireEvent.click(clear);
+    expect(runningTotal.textContent).toEqual("0");
+  });
 });
